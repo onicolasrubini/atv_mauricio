12677fc8dc28565825556a6d5915f45bc395b665
Niicolas <nicollasrubini@gmail.com>
2023-08-18 17:09:09 -0400

    Update func_tercerizado

--- func_tercerizado.py
+++ func_tercerizado.py
@@ -2,11 +2,15 @@ class Funcionario():
     
  
 
-    def __init__(self,nome,h_trabalhadas,valor_porH,despesa_add):
+    def __init__(self,nome,nomeTercerizado,h_trabalhadas,h_trabalhadasTercerizado,valor_porH,valor_porH_ter,despesa_add):
         self.nome = nome
+        self.nomeTercerizado = nomeTercerizado
         self.h_trabalhadas = h_trabalhadas
+        self.h_trabalhadasTercerizado = h_trabalhadasTercerizado
         self.valor_porH = valor_porH
+        self.valor_porH_ter = valor_porH_ter
         self.despesa_add = despesa_add
+        
         
         
     def func(self,h_trabalhadas,valor_porH):
