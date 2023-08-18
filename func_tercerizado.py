0a1f9f285f6deeda4a088e6bd743caa627df99a4
Niicolas <nicollasrubini@gmail.com>
2023-08-18 16:09:01 -0400

    Update func_tercerizado
    orientação a objeto do exercicio funcionario

---
+++ func_tercerizado.py
@@ -0,0 +1,20 @@
+class funcionario():
+    
+    
+    def __init__(self,nome,h_trabalhadas,valor_porH,despesa_add):
+        self.nome = nome
+        self.h_trabalhadas = h_trabalhadas
+        self.valor_porH = valor_porH
+        self.despesa_add = despesa_add
+        
+        
+    def func(self,h_trabalhadas,valor_porH):
+        pagamento = h_trabalhadas * valor_porH
+        print(pagamento)
+        
+        
+    def func_tercerizado(self,h_trabalhadas,valor_porH,despesa_add):
+        adicional = 110/100 * despesa_add
+        pagamento = h_trabalhadas * valor_porH + adicional
+        print(pagamento)
+        
