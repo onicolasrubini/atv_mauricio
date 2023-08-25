2ff72a1f36ad2b26b9a88c2a8a0bb7f88282188d
Dudu1705 <eduardoromanoamaral11@gmail.com>
2023-08-25 14:27:46 -0400

    telaini e cadastro do produto

---
+++ cadastroproduto.py
@@ -0,0 +1,101 @@
+
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QLineEdit,
+    QMainWindow, QSizePolicy, QVBoxLayout, QWidget)
+import cadastroproduto
+
+class Ui_Cadastro(object):
+    def setupUi(self, Cadastro):
+        if not Cadastro.objectName():
+            Cadastro.setObjectName(u"Cadastro")
+        Cadastro.resize(800, 600)
+        font = QFont()
+        font.setPointSize(10)
+        Cadastro.setFont(font)
+        self.centralwidget = QWidget(Cadastro)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout_2 = QVBoxLayout(self.centralwidget)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.f_principal = QFrame(self.centralwidget)
+        self.f_principal.setObjectName(u"f_principal")
+        self.f_principal.setFrameShape(QFrame.StyledPanel)
+        self.f_principal.setFrameShadow(QFrame.Raised)
+        self.verticalLayout = QVBoxLayout(self.f_principal)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.lbl_cadastrop = QLabel(self.f_principal)
+        self.lbl_cadastrop.setObjectName(u"lbl_cadastrop")
+        font1 = QFont()
+        font1.setPointSize(17)
+        self.lbl_cadastrop.setFont(font1)
+
+        self.verticalLayout.addWidget(self.lbl_cadastrop)
+
+        self.lbl_nome = QLabel(self.f_principal)
+        self.lbl_nome.setObjectName(u"lbl_nome")
+        self.lbl_nome.setFont(font)
+
+        self.verticalLayout.addWidget(self.lbl_nome)
+
+        self.input_nome = QLineEdit(self.f_principal)
+        self.input_nome.setObjectName(u"input_nome")
+
+        self.verticalLayout.addWidget(self.input_nome)
+
+        self.lbl_precouni = QLabel(self.f_principal)
+        self.lbl_precouni.setObjectName(u"lbl_precouni")
+        self.lbl_precouni.setFont(font)
+
+        self.verticalLayout.addWidget(self.lbl_precouni)
+
+        self.input_precouni = QLineEdit(self.f_principal)
+        self.input_precouni.setObjectName(u"input_precouni")
+
+        self.verticalLayout.addWidget(self.input_precouni)
+
+        self.lbl_qntdestq = QLabel(self.f_principal)
+        self.lbl_qntdestq.setObjectName(u"lbl_qntdestq")
+
+        self.verticalLayout.addWidget(self.lbl_qntdestq)
+
+        self.input_qntdestq = QLineEdit(self.f_principal)
+        self.input_qntdestq.setObjectName(u"input_qntdestq")
+
+        self.verticalLayout.addWidget(self.input_qntdestq)
+
+
+        self.verticalLayout_2.addWidget(self.f_principal)
+
+        Cadastro.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(Cadastro)
+
+        QMetaObject.connectSlotsByName(Cadastro)
+    # setupUi
+
+    def retranslateUi(self, Cadastro):
+        Cadastro.setWindowTitle(QCoreApplication.translate("Cadastro", u"MainWindow", None))
+        self.lbl_cadastrop.setText(QCoreApplication.translate("Cadastro", u"Cadastro do Produto", None))
+        self.lbl_nome.setText(QCoreApplication.translate("Cadastro", u"Nome", None))
+        self.input_nome.setPlaceholderText(QCoreApplication.translate("Cadastro", u"Digite o nome do produto", None))
+        self.lbl_precouni.setText(QCoreApplication.translate("Cadastro", u"Pre\u00e7o Unit\u00e1rio", None))
+        self.input_precouni.setPlaceholderText(QCoreApplication.translate("Cadastro", u"Digite o pre\u00e7o unit\u00e1rio", None))
+        self.lbl_qntdestq.setText(QCoreApplication.translate("Cadastro", u"Quantidade de estoque", None))
+        self.input_qntdestq.setPlaceholderText(QCoreApplication.translate("Cadastro", u"Digite a quantidade de estoque", None))
+    # retranslateUi
+
+
+if __name__=='__main__':
+    import sys
+    app = QApplication(sys.argv)
+    w=QMainWindow()
+    janela=Ui_Cadastro()
+    janela.setupUi(w)
+    w.show()
+    app.exec()
---
+++ telaini.py
@@ -0,0 +1,78 @@
+
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QMainWindow,
+    QPushButton, QSizePolicy, QVBoxLayout, QWidget)
+import telaini
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 602)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout_2 = QVBoxLayout(self.centralwidget)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.f_principal = QFrame(self.centralwidget)
+        self.f_principal.setObjectName(u"f_principal")
+        self.f_principal.setFrameShape(QFrame.StyledPanel)
+        self.f_principal.setFrameShadow(QFrame.Raised)
+        self.verticalLayout = QVBoxLayout(self.f_principal)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.lbl_telainicial = QLabel(self.f_principal)
+        self.lbl_telainicial.setObjectName(u"lbl_telainicial")
+        font = QFont()
+        font.setPointSize(30)
+        self.lbl_telainicial.setFont(font)
+
+        self.verticalLayout.addWidget(self.lbl_telainicial, 0, Qt.AlignHCenter)
+
+        self.pushButton = QPushButton(self.f_principal)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.verticalLayout.addWidget(self.pushButton)
+
+        self.btn_estoque = QPushButton(self.f_principal)
+        self.btn_estoque.setObjectName(u"btn_estoque")
+
+        self.verticalLayout.addWidget(self.btn_estoque)
+
+        self.btn_cadastrarp = QPushButton(self.f_principal)
+        self.btn_cadastrarp.setObjectName(u"btn_cadastrarp")
+
+        self.verticalLayout.addWidget(self.btn_cadastrarp)
+
+
+        self.verticalLayout_2.addWidget(self.f_principal)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.lbl_telainicial.setText(QCoreApplication.translate("MainWindow", u"Tela Inicial", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Comprar", None))
+        self.btn_estoque.setText(QCoreApplication.translate("MainWindow", u"Ver Estoque", None))
+        self.btn_cadastrarp.setText(QCoreApplication.translate("MainWindow", u"Cadastrar Produto", None))
+    # retranslateUi
+
+
+if __name__=='__main__':
+    import sys
+    app = QApplication(sys.argv)
+    w=QMainWindow()
+    janela=Ui_MainWindow()
+    janela.setupUi(w)
+    w.show()
+    app.exec()
