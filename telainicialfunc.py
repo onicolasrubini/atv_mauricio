684b4e27feb1d59113269d893a94dfed80f056df
Dudu1705 <eduardoromanoamaral11@gmail.com>
2023-08-28 13:57:29 -0400

    update

---
+++ telainicialfunc.py
@@ -0,0 +1,63 @@
+
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QMainWindow,
+    QPushButton, QSizePolicy, QVBoxLayout, QWidget)
+import telainicialfunc
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.f_principal = QFrame(self.centralwidget)
+        self.f_principal.setObjectName(u"f_principal")
+        self.f_principal.setFrameShape(QFrame.StyledPanel)
+        self.f_principal.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_2 = QVBoxLayout(self.f_principal)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.lb_telainicial = QLabel(self.f_principal)
+        self.lb_telainicial.setObjectName(u"lb_telainicial")
+        font = QFont()
+        font.setPointSize(30)
+        self.lb_telainicial.setFont(font)
+
+        self.verticalLayout_2.addWidget(self.lb_telainicial, 0, Qt.AlignHCenter)
+
+        self.btn_cadastrofp = QPushButton(self.f_principal)
+        self.btn_cadastrofp.setObjectName(u"btn_cadastrofp")
+
+        self.verticalLayout_2.addWidget(self.btn_cadastrofp)
+
+        self.btn_cadastroft = QPushButton(self.f_principal)
+        self.btn_cadastroft.setObjectName(u"btn_cadastroft")
+
+        self.verticalLayout_2.addWidget(self.btn_cadastroft)
+
+
+        self.verticalLayout.addWidget(self.f_principal)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.lb_telainicial.setText(QCoreApplication.translate("MainWindow", u"Tela Inicial", None))
+        self.btn_cadastrofp.setText(QCoreApplication.translate("MainWindow", u"Cadastro funcion\u00e1rio pr\u00f3prio", None))
+        self.btn_cadastroft.setText(QCoreApplication.translate("MainWindow", u"Cadastro funcion\u00e1rio tercerizado", None))
+    # retranslateUi
+
