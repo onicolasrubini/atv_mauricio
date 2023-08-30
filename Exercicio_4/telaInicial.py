788354d94c6408660af46eae2d82b38e36bdfa63
jotafreitas <silvahentony38@gmail.com>
2023-08-30 17:17:35 -0400

    continuação att

--- Exercicio_4/telaInicial.py
+++ Exercicio_4/telaInicial.py
@@ -1,8 +1,13 @@
 import sys
 from PySide6.QtGui import QPixmap
-from PySide6.QtCore import QSize, Qt
+from PySide6.QtCore import Qt
 from datetime import datetime, timedelta
-from PySide6.QtWidgets import QApplication, QMainWindow, QLineEdit, QCheckBox, QPushButton, QVBoxLayout, QLabel, QDialog, QWidget, QFrame, QTableView, QTableWidget
+from PySide6.QtWidgets import (
+    QApplication, QMainWindow, QPushButton, 
+    QVBoxLayout, QLabel, QWidget, QFrame, 
+    QTableView, QTableWidget, QTableWidgetItem,
+    QDockWidget, QFormLayout, QLineEdit, QSpinBox,
+)
 
 from cadastro import Fila
 
@@ -14,6 +19,7 @@ class tela_inicial(QMainWindow):
 
         self.central_widget = QWidget() # Para centralizar
         self.setCentralWidget(self.central_widget)
+        
         
         # para colocar a imagem 
         self.image_frame = QFrame(self)
@@ -37,18 +43,39 @@ class tela_inicial(QMainWindow):
         escala = pixmap.scaled(500, 400)
         self.lbl_img.setPixmap(escala)
         
+        
         # Titulo 
         self.lbl_Titulo_tela_inicial = QLabel('Fila de pacientes',self)
         self.lbl_Titulo_tela_inicial.setGeometry(215,200,100,30)   
-        
+ 
+ 
         # Botão de cadastro
         self.btn_cadastro = QPushButton('Cadastrar-se',self)
         self.btn_cadastro.setGeometry(126,600,250,30)
         self.btn_cadastro.clicked.connect(self.cadastro)
  
-    def tabela_saida(self):
-        print('a')
+ #===========================================================================================
+    def limpar_dados(self):
+        self.input_nomet.setText("")
+        self.input_hrstt.setText("")
+        self.input_valorhrst.setText("")
+        self.input_despesa.setText("")
+    
+    
+    def registrar(self):
+        nome_tercerizado = self.input_nomet.text()
         
+        funcionario = Lista(nome_tercerizado)
+        self.lista_ter.append(funcionario)
+        self.update_funcionario_tercerizado()
+        self.input_nomet.clear()
+        
+        
+    def update_funcionario_tercerizado(self):
+        nome_func = [f"{funcionario.nome}" for funcionario in self.lista_ter]
+        self.modelo_de_func.setStringList(nome_func)
+        
+ #===========================================================================================       
     # def para ir para a proxima tela
     def cadastro(self):       
         self.secondary_window = Fila()
